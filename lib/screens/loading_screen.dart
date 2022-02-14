f41599519d880a87b99bb8193c2fa552c3fa7a62
Abdulhamid Oyedele Oyempemi <oyempemia@gmail.com>
2022-02-14 15:23:27 +0100

    JSON parsing and dynamic data types from our api call response. JSON object de serialization.

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,6 +1,7 @@
 import 'package:clima/services/location.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -17,8 +18,25 @@ class _LoadingScreenState extends State<LoadingScreen> {
 
   void getData() async {
     http.Response response = await http.get(Uri.parse(
-        'https://api.openweathermap.org/data/2.5/weather?lat={35}&lon={139}&appid={8c258277bbfd64f54a4b357323f6ce1f}'));
-    print(response.body);
+        'https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=8c258277bbfd64f54a4b357323f6ce1f'));
+
+    if (response.statusCode == 200) {
+      String data = response.body;
+      /*var longitude = jsonDecode(data)['coord']['lon'];
+      var weatherDescription = jsonDecode(data)['weather'][0]['description'];*/
+
+      var decodedData = jsonDecode(data);
+
+      int weatherCondition = decodedData['weather'][0]['id'];
+      double temp = decodedData['main']['temp'];
+      String city = decodedData['name'];
+
+      print(weatherCondition);
+      print(temp);
+      print(city);
+    } else {
+      print(response.statusCode);
+    }
   }
 
   @override
